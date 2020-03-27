57e975011eb8b968954ae9f0af49ff97dd469352
James Thistlewood <jamesthistlewood@gmail.com>
2020-03-27 10:46:50 +0000

    allow joining game

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,7 +1,20 @@
 const crypto = require('crypto');
 const db = require('./database.js');
+const config = require('./config.js');
 
-// Provide resolver functions for your schema fields
+function validateUserHash(hash) {
+  return new Promise((resolve, reject) => {
+    db.query(
+      'SELECT COUNT(*) FROM users WHERE hash = ?',
+      [hash],
+      (error, results) => {
+        const count = results[0]['COUNT(*)'];
+        resolve(count === 1);
+      }
+    );
+  });
+}
+
 const resolvers = {
   Query: {
     hello: () => 'Hello world!',
@@ -16,6 +29,52 @@ const resolvers = {
       ]);
 
       return hash;
+    },
+    createGame: (parent, args, context, info) => {
+      const joinCode = `000${Math.floor(Math.random() * 10000)}`.slice(-4);
+      const hash = crypto.randomBytes(32).toString('hex');
+      const players = JSON.stringify([args.userHash]);
+      db.query(
+        'INSERT INTO games (hash, joinCode, players, stage) VALUES (?, ?, ?, 1)',
+        [hash, joinCode, players]
+      );
+
+      return hash;
+    },
+    joinGame: (parent, args, context, info) => {
+      return new Promise(async (resolve, reject) => {
+        if (!(await validateUserHash(args.userHash))) {
+          console.log('invalid user hash');
+          return resolve(null);
+        }
+        db.query(
+          'SELECT hash, players FROM games WHERE joinCode = ? AND stage != 0',
+          [args.gameCode],
+          (error, results) => {
+            if (results.length !== 1) {
+              console.log('game not found');
+              return resolve(null);
+            }
+
+            /* Add player to list of players */
+            const game = results[0];
+            const players = JSON.parse(game.players);
+            if (players.length >= config.maxPlayers) {
+              /* TODO allow join to spectate? */
+              console.log('too many players');
+              return resolve(null);
+            } else if (!players.includes(args.userHash)) {
+              players.push(args.userHash);
+              db.query('UPDATE games SET players = ? WHERE hash = ?', [
+                JSON.stringify(players),
+                game.hash,
+              ]);
+            }
+
+            return resolve(game.hash);
+          }
+        );
+      });
     },
   },
 };
--- src/schema.js
+++ src/schema.js
@@ -1,14 +1,17 @@
 const { gql } = require('apollo-server-express');
 
-// Construct a schema, using GraphQL schema language
 const typeDefs = gql`
   type Query {
     hello: String
   }
 
   type Mutation {
-    "Create a user, returns nickname"
+    "Create a user, returns user hash"
     createUser(nickname: String!): String!
+    "Create a new game, returns a game hash"
+    createGame(userHash: String!): String!
+    "Join an existing game, returns game hash"
+    joinGame(userHash: String!, gameCode: String!): String
   }
 `;
 
