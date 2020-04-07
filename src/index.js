4237ed848bc95ab1c8d8bd8b0af4deeba391d819
James Thistlewood <jamesthistlewood@gmail.com>
2020-04-07 10:15:55 +0100

    attempt to fix homepage only bug

--- src/index.js
+++ src/index.js
@@ -24,9 +24,6 @@ app.use(cors());
 if (process.env.NODE_ENV === 'production') {
   console.log('In production, serving from static');
   app.use(express.static(path.join(__dirname, 'client')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/index.html'));
-  });
 }
 
 server.applyMiddleware({
